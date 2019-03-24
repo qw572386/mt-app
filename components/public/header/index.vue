2d3db30f818f949460dc4ff079765b6a6b514c4c
qw572386 <735520121@qq.com>
2019-03-25 07:52:07 +0800

    首页顶部栏

---
+++ components/public/header/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="m-header">
+        <el-row>
+            <el-col>
+                <top-bar />
+            </el-col>
+        </el-row>
+        <el-row>
+            <el-col>
+                <search-bar />
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+import TopBar from './topBar'
+import SearchBar from './searchBar'
+export default {
+    components: {
+        TopBar,
+        SearchBar
+    }
+}
+</script>
+
+<style lang="scss">
+    @import '@/assets/css/public/header.scss';
+</style>
