e0bed443656f13e02c7cebc6a2ecfa1140a72204
qw572386 <735520121@qq.com>
2019-04-29 23:45:25 +0800

    购物车页订单页完成

--- pages/cart.vue
+++ pages/cart.vue
@@ -25,16 +25,16 @@ export default {
   data() {
     return {
       cart: [
-        {
-          name: "王小虎",
-          price: 100,
-          count: 1
-        },
-        {
-          name: "王小虎",
-          price: 200,
-          count: 1
-        },
+        // {
+        //   name: "王小虎",
+        //   price: 100,
+        //   count: 1
+        // },
+        // {
+        //   name: "王小虎",
+        //   price: 200,
+        //   count: 1
+        // },
       ]
     };
   },
@@ -48,7 +48,38 @@ export default {
     }
   },
   methods: {
-    submit() {}
+    async submit() {
+      let { status, data: { code, id } } = await this.$axios.post('/order/createOrder', {
+        count: this.cart[0].count,
+        price: this.cart[0].price,
+        id: this.cartNo
+      })
+      if (status === 200 && code === 0) {
+        this.$alert(`恭喜您，已成功下单，订单号： ${id}， 下单成功`, {
+          confirmButtonText: '确定',
+          callback: action => {
+            location.href = '/order'
+          }
+        })
+      }
+    }
+  },
+  async asyncData(ctx) {
+    let { status, data: { code, data: { name, price } } } = await ctx.$axios.post('/cart/getCart', {
+      id: ctx.query.id
+    })
+    if (status === 200 && code === 0 && name) {
+      return {
+        cart: [
+          {
+            name,
+            price,
+            count: 1
+          }
+        ],
+        cartNo: ctx.query.id
+      }
+    }
   }
 };
 </script>
